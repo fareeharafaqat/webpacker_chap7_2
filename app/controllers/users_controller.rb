a13849d8914ef54aac41ebb41cdffc6ef98a5547
fareeharafaqat <73890039+fareeharafaqat@users.noreply.github.com>
2020-12-18 18:37:10 +0500

    user_ctrl updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        #format.html { redirect_to "#{users_url}/#{@user}".gsub("http","https"),  notice: 'User was successfully created.'}
-        format.html { redirect_to @user,  notice: 'User was successfully created.'}
+        format.html { redirect_to "#{users_url}/#{@user}".gsub("http","https"),  notice: 'User was successfully created.'}
+        #format.html { redirect_to @user,  notice: 'User was successfully created.'}
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -43,7 +43,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user,  notice: 'User was successfully updated.'}
+        #format.html { redirect_to @user,  notice: 'User was successfully updated.'}
+        format.html { redirect_to "#{users_url}/#{@user}".gsub("http","https"),  notice: 'User was successfully created.'}
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -57,8 +58,8 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      #format.html { redirect_to "#{users_url}/".gsub("http","https"),  notice: 'User was successfully destroyed.'}
-      format.html { redirect_to users_url,  notice: 'User was successfully destroyed.'}
+      format.html { redirect_to "#{users_url}/".gsub("http","https"),  notice: 'User was successfully destroyed.'}
+      #format.html { redirect_to users_url,  notice: 'User was successfully destroyed.'}
       format.json { head :no_content }
     end
   end
